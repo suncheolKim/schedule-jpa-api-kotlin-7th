cf7e5a4a27f8d89932256802d8e5d721b58e9442
Suncheol Kim <tinymin@yahoo.co.kr>
2025-05-22 17:32:21 +0900

    first commit

---
+++ src/main/java/net/sckim/schedule/api/ScheduleJpaApiApplication.java
@@ -0,0 +1,13 @@
+package net.sckim.schedule.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleJpaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleJpaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/sckim/schedule/api/ScheduleJpaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sckim.schedule.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleJpaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
