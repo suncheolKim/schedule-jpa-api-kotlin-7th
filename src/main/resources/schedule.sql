63dab5a20893d8e4e5becb35ad8a4398dc89e1fa
Suncheol Kim <tinymin@yahoo.co.kr>
2025-05-23 18:41:22 +0900

    feat: Lv7. 댓글 CRUD - 댓글 테이블 DDL schedule.sql에 추가

--- src/main/resources/schedule.sql
+++ src/main/resources/schedule.sql
@@ -10,7 +10,7 @@ CREATE TABLE `user`
 
 CREATE TABLE `schedule`
 (
-    `id`         BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT '일정 ID',
+    `id`         BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'ID',
     `user_id`    BIGINT             NOT NULL COMMENT '작성자 ID',
     `title`      VARCHAR(10)        NOT NULL COMMENT '제목',
     `contents`   VARCHAR(500)       NOT NULL COMMENT '내용',
@@ -18,4 +18,17 @@ CREATE TABLE `schedule`
     `updated_at` DATETIME           NOT NULL COMMENT '수정일시',
 
     CONSTRAINT `fk_schedule_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+);
+
+CREATE TABLE `comment`
+(
+    `id`          BIGINT PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `user_id`     BIGINT             NOT NULL COMMENT '작성자 ID',
+    `schedule_id` BIGINT             NOT NULL COMMENT '일정 ID',
+    `contents`    VARCHAR(500)       NOT NULL COMMENT '내용',
+    `created_at`  DATETIME           NOT NULL COMMENT '생성일시',
+    `updated_at`  DATETIME           NOT NULL COMMENT '수정일시',
+
+    CONSTRAINT `fk_comment_schedule` FOREIGN KEY (`schedule_id`) REFERENCES `schedule` (`id`),
+    CONSTRAINT `fk_comment_user` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
 );
